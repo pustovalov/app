b0399939b42ce38f5061d6d5b147af57d2e45964
Pavel <pustovalov.pavel@gmail.com>
2015-10-09 22:13:31 -0700

    Use seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/most-common-german-words-2"))
+
+lines = page.css("table tr:not(:first-child)")
+
+lines.each do |line|
+  original_text = line.css("td:nth-child(2)").text
+  translated_text = line.css("td:nth-child(3)").text
+  Card.create(original_text: original_text, translated_text: translated_text, review_date: Date.today + 3.days)
+end
+
+
