70e181bd9e1a627905424b2ed00ceec932112670
Pavel <pustovalov.pavel@gmail.com>
2015-10-12 02:07:20 -0700

    Polish

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,19 +1,16 @@
 class MainController < ApplicationController
-  
   def index
-    @card = Card.expired.random    
+    @card = Card.expired.random
   end
   
   def check
     @card = Card.where(original_text: params[:card][:original_text]).take
     user_text = params[:card][:translated_text]
-    unless @card.blank?
-      @card.update(review_date: Time.zone.today + 3.days)
-      if @card.translated_text == user_text
-        flash[:notice] = "Правильно"
-      else
-        flash[:error] = "Не правильно"
-      end
+    @card.update(review_date: Time.zone.today + 3.days)
+    if @card.translated_text == user_text
+      flash[:notice] = "Правильно"
+    else
+      flash[:error] = "Не правильно"
     end
     redirect_to action: "index"
   end
