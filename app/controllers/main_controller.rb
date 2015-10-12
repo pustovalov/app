efec0a7c9bc38d5a990758e4f4d77c9454e87cc0
Pavel <pustovalov.pavel@gmail.com>
2015-10-12 00:49:33 -0700

    Show one random expired card

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,20 @@
 class MainController < ApplicationController
+  
   def index
+    @card = Card.expired.random    
+  end
+  
+  def check
+    @card = Card.where(original_text: params[:card][:original_text]).take
+    user_text = params[:card][:translated_text]  
+    if !@card.blank?
+      @card.update(review_date: Date.today + 3.days)
+      if @card.translated_text.include? user_text
+        flash[:notice] = 'Правильно'
+      else
+        flash[:error] = 'Не правильно'
+      end
+    end
+    redirect_to action: 'index'
   end
 end
