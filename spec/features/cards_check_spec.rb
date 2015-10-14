318b5af8c363beecf431baea97f4e15ce3f9d235
Pavel <pustovalov.pavel@gmail.com>
2015-10-14 00:56:03 -0700

    Add feature test for cards check

---
+++ spec/features/cards_check_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+feature "Check cards" do
+  
+  context "cards are available for training" do
+
+    before do
+      @card = create(:card)
+      visit root_path
+    end
+    
+    scenario "opens successfully" do
+      expect(page.status_code).to be 200
+    end
+    
+    scenario "enter valid translation" do      
+      fill_in 'card_translated_text', with: @card.translated_text
+      create(:card, original_text: "3213213", translated_text: "312312")
+      find(".ac-check-submit").click
+      expect(page).to have_content "Правильно"
+    end
+    
+    scenario "enter invalid translation" do      
+      fill_in 'card_translated_text', with: "132"
+      create(:card, original_text: "3213213", translated_text: "312312")
+      find(".ac-check-submit").click
+      expect(page).to have_content "Не правильно"
+    end
+  end
+  
+  context "cards are not available for training" do
+    before do
+      visit root_path
+    end
+    
+    scenario "opens successfully" do
+      expect(page.status_code).to be 200
+    end
+    
+    scenario "no available cards" do      
+      expect(page).to have_content "Нету доступных карточек, приходи позже."
+    end
+  end
+end
