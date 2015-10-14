a1b65a1a24a9f3b3651ea4e62a63a00671327aab
Pavel <pustovalov.pavel@gmail.com>
2015-10-14 01:01:18 -0700

    Polish

--- test/factories/cards.rb
+++ test/factories/cards.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     original_text "du"
     translated_text "you"
     review_date { Time.zone.now - 5.days }
-  
+
     trait :not_expired do
       review_date { Time.zone.now + 5.days }
     end
