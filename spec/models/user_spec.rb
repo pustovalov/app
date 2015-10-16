8e0fa52c3ab2e9a2cb545eee41379a2a8c82a86b
Pavel <pustovalov.pavel@gmail.com>
2015-10-15 18:56:51 -0700

    Cleanup and polish user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,18 @@ require "rails_helper"
 
 describe User do
   let!(:user) { create(:user) }
+  let!(:card1) { user.cards.create(build(:card).attributes) }
+  let!(:card2) { user.cards.create(build(:card, original_text: "3123").attributes) }
 
   it "has a valid factory" do
     expect(build :user).to be_valid
   end
 
   it "has many card" do
-    user.cards.create(build(:card).attributes)
-    user.cards.create(build(:card, original_text: "3123").attributes)
     expect(user.cards.count).to eq(2)
   end
 
   it "removes cards with user" do
-    user.cards.create(build(:card).attributes)
-    user.cards.create(build(:card, original_text: "3123").attributes)
     user.destroy
     expect(user.cards.count).to eq(0)
   end
