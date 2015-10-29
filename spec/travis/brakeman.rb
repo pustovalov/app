8a2066e39e54222edfab0d47915c02613ec497ac
Pavel <pustovalov.pavel@gmail.com>
2015-10-28 22:34:49 -0700

    Polish

--- spec/travis/brakeman.rb
+++ spec/travis/brakeman.rb
@@ -1,12 +1,10 @@
-require 'brakeman'
+require "brakeman"
 tracker = Brakeman.run app_path: "", quiet: false
 
 puts tracker.report
 if tracker.filtered_warnings.length === 0
   puts "Brakeman did not find any new vulnerabilities."
 else
-  tracker.filtered_warnings.each { |warn|
-    puts warn
-  }
+  tracker.filtered_warnings.each { |warn| puts warn }
   raise "Brakeman found vulnerabilities!!!!"
 end
