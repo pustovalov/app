da3a726103fa96090481d192127c31ccd63f5355
Pavel <pustovalov.pavel@gmail.com>
2015-11-01 10:04:24 -0700

    Add default value for check table

--- db/migrate/20151110014740_add_checks_table.rb
+++ db/migrate/20151110014740_add_checks_table.rb
@@ -1,8 +1,8 @@
 class AddChecksTable < ActiveRecord::Migration
   def change
     create_table :checks do |t|
-      t.integer :times_reviewed
-      t.integer :losing_streak
+      t.integer :times_reviewed, null: false, default: 0
+      t.integer :losing_streak, null: false, default: 0
 
       t.timestamps null: false
     end
